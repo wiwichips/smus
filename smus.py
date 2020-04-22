5717a12a5e63c4ffa76d29df7691640d106687b3
wiwichips <wpringle@uoguelph.ca>
2020-04-21 20:48:28 -0400

    Added first get request in python to get from lastfm API

--- smus.py
+++ smus.py
@@ -1,8 +1,32 @@
-#import the argparse library
-import argparse
-
+#imports
+import argparse # for parsing command line arguments
+import requests # for get requests
+import json
 import os
 import sys
+
+## functions
+# function for lastfm get requests
+def lastfm_get(method):
+	# define headers and URL
+    headers = {'user-agent': 'smus'}
+    url = 'http://ws.audioscrobbler.com/2.0/'
+
+    # Add API key and format to the payload
+    payload = {
+		'api_key': 'ad442a0e29b05a2834314212926fa974',
+		'user': 'wiwichips',
+		'method': method,
+		'format': 'json'
+	}
+
+    response = requests.get(url, headers=headers, params=payload)
+    return response
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
 #create the parser
 my_parser = argparse.ArgumentParser(description='Data visualization of music listening habbits.\nPowered by the Last.fm API.',
@@ -17,5 +41,10 @@ from ctypes import *
 library = "./libbarchart.so"
 barChart = CDLL(library)
 
-barChart.driver()
+# call the driver function for testing
+#barChart.driver()
+
+# Call lastfm API
+r = lastfm_get('user.getInfo');
+jprint(r.json()['user']['playcount'])
 
